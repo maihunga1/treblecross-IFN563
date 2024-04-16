37312c5c061e1c9081c856fe2817107bb8e44740
Khoi Tuan <khoituan1412@gmail.com>
2024-04-16 19:00:39 +1000

    move TrebleCrossGameController to its own file

--- BoardGame-main/Src/GameController.cs
+++ BoardGame-main/Src/GameController.cs
@@ -237,46 +237,4 @@ public class GameController
 
     Console.WriteLine("Saved game snapshot as data.json");
   }
-
-}
-
-/// <summary>
-/// Game controller for Treble Cross game
-/// </summary>
-public class TrebleCrossController : GameController
-{
-  public TrebleCrossController(Player[] players, Board board, int playerTurn, bool opponentIsBot, Record record)
-    : base(players, board, playerTurn, opponentIsBot, record)
-  {
-  }
-
-  public TrebleCrossController(bool opponentIsBot, int boardSize)
-    : base(opponentIsBot, boardSize)
-  {
-  }
-
-  /// <summary>
-  /// A method to check winning condition from the current game state
-  /// </summary>
-  public override bool CheckWin()
-  {
-    int countSequence = 0;
-
-    for (int col = 0; col < Board.CurrentBoard[0].Length; col++)
-    {
-      if (Board.CurrentBoard[0][col] == 1)
-      {
-        countSequence++;
-        if (countSequence == 3)
-        {
-          return true;
-        }
-      }
-      else
-      {
-        countSequence = 0;
-      }
-    }
-    return false;
-  }
 }
